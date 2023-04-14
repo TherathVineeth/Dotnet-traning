90da6b499d5e381f344512824cc75ca0710bb7e8
TherathVineeth <129932487+TherathVineeth@users.noreply.github.com>
2023-04-14 09:24:49 +0530

    Add files via upload

---
+++ Day4/String/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Write a program in C# Sharp
+             *  to count a total number of alphabets, digits and 
+             *  special characters in a string  */
+
+            Console.WriteLine("Enter the string");
+            string s = Console.ReadLine();
+            String(s);
+
+        }
+
+        private static void String(string s)
+        {
+            int n = s.Length;
+            int count = 0;
+            int num = 0;
+            int spl = 0;
+            for(int i=0; i < n; i++)
+            {
+                if (((s[i] >= 'a') && (s[i] <= 'z')) || ((s[i] >= 'A') && (s[i] <= 'Z')))
+                {
+                    count++;
+                } 
+                else if((s[i] >='0') && (s[i] <= '9'))
+                {
+                    num++;
+                }
+                else
+                {
+                    spl++;
+                }
+
+            }
+
+            Console.WriteLine($"Number of alphabets in string : {count}");
+            Console.WriteLine($"Count of numbers in given string: {num}");
+            Console.WriteLine($"Number of special char : {spl}");
+
+        }
+    }
+}
---
+++ Day4/Swap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number 1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("number 2");
+            int n2 = int.Parse(Console.ReadLine());
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+    }
+}
