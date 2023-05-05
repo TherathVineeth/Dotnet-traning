94f544d93d6f6d75df04e33fe30b51769184546a
TherathVineeth <129932487+TherathVineeth@users.noreply.github.com>
2023-05-05 09:16:11 +0530

    Add files via upload

---
+++ Day22/ABCD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<emp> e = new List<emp>();
+
+            e.Add(new emp ("Hari","chennai"));
+            e.Add(new emp ("susil","madurai"));
+            e.Add(new emp ("Akhil","Banglore"));
+            e.Add(new emp ("Ram","Kerala"));
+            e.Add(new emp ("Rahul","chennai"));
+            e.Add(new emp ("Mohan","Goa"));
+            e.Add(new emp ("Karthik","Sikkim"));
+            e.Add(new emp ("Keerthana","Maharashtra"));
+            
+            
+            var item = from emp a in e orderby a._place group a by a._place;
+            foreach(var p in item)
+            {
+                Console.WriteLine("Employee with "+p.Key+"  City");
+                foreach(var c in p)
+                {
+                    Console.WriteLine("   " + c._name);
+                }
+            }
+
+        }
+    }
+}
---
+++ Day22/ABCD/emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCD
+{
+    internal class emp
+    {
+        public string _name;
+        public string _place;
+        public emp() { }
+        public emp(string na,string place)
+        {
+            _name = na;
+            _place = place;
+        }
+    }
+}
