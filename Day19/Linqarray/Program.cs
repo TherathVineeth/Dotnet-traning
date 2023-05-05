b93506bc9503a104af5ca1e16a6bbc0acfdfe2c8
TherathVineeth <129932487+TherathVineeth@users.noreply.github.com>
2023-05-05 09:12:49 +0530

    Add files via upload

---
+++ Day19/Linqarray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] flowers = { "dailya","rose","lotus","lilly","hybiscus" };
+            var item = from flower in flowers where flower.StartsWith("d") select flower;
+            foreach(string f in item)
+            {
+                Console.WriteLine(f);
+            }
+
+            var it = from flow in flowers let len = flow.Length where len>4 select flow;
+            foreach (string f in it)
+            {
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
