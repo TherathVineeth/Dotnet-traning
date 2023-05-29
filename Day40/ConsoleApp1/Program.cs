c4796a1648a49d5d7c6a3b450bcd4debabf1069c
TherathVineeth <129932487+TherathVineeth@users.noreply.github.com>
2023-05-29 09:27:59 +0530

    Add files via upload

---
+++ Day40/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string s = Console.ReadLine();
+            for(int i=0; i< 4;i++)
+            {
+                Console.Write(s[0]);
+                Console.Write(s[1]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
