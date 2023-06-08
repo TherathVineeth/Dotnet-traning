440a1a852369da985271b1fe8056fcacd146dcbf
TherathVineeth <129932487+TherathVineeth@users.noreply.github.com>
2023-06-08 16:56:28 +0530

    Add files via upload

---
+++ Day46/hm/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace hm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+            Console.WriteLine("Enter the row");
+            int n = int.Parse(Console.ReadLine());
+            int s = n, st = n;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 1; j <= s-1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = s-1; k < st ; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                s--;
+            }
+        }
+    }
+}
